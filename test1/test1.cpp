7806e69cb70844d86106bd418756714500c88553
Marin-Giuliana <maringiuliana21@gmail.com>
2025-02-25 18:37:06 +0200

    adaugare 2

--- test1/test1.cpp
+++ test1/test1.cpp
@@ -1,7 +1,7 @@
-#include <stdio.h>
-
-int main() 
-{
-    printf("Salut dubla 2, GitHub!\n");
-    return 0;
-}
+//#include <stdio.h>
+//
+//int main() 
+//{
+//    printf("Salut dubla 2, GitHub!\n");
+//    return 0;
+//}
